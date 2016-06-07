11d2b4a38d7011c09b8e56374ef6d8d7e6a3807c
Dpappy <fjordbenson@gmail.com>
2016-06-07 14:44:24 -0700

    edited js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Change Selector if necessary
+$('html').keypress(function(e){
+  
+  if (e.keyCode === 32) {
+     
+$('.fullsize img'):target {
+  opacity: 1;
+    width: 60%;
+    top-margin:120px;
+    left-margin:100px;
+ 
+  
+
+console.log(e.keyCode); 
+  }
+});
