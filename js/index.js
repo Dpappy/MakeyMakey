101515294659aa769e5ef70307b9c88e71655a77
Dpappy <fjordbenson@gmail.com>
2016-06-07 15:19:02 -0700

    edit page

--- js/index.js
+++ js/index.js
@@ -1,16 +1,2 @@
-// Change Selector if necessary
-$('html').keypress(function(e){
-  
-  if (e.keyCode === 32) {
-     
-$('.fullsize img'):target {
-  opacity: 1;
-    width: 60%;
-    top-margin:120px;
-    left-margin:100px;
- 
-  
+// Stuff Before Event
 
-console.log(e.keyCode); 
-  }
-});
