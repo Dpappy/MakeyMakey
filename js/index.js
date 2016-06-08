7090ce59f12661c5bc492c0e36e32b8d47079ad7
Dpappy <fjordbenson@gmail.com>
2016-06-07 23:58:46 -0700

    edited content

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // Stuff Before Event
 
+$('html').keypress( function(e){
+  
+  if (e.keyCode === 32) {
+   
+    // Stuff During Event
+   console.log(e.keyCode);
+    
+  }
+  
+  // Stuff After Event
+  
+});
